b278a229a3645c6ce1cdffd37d8ed440773bd39a
GamerBene19 <nerb.benedikt@gmail.com>
2022-02-02 23:27:41 +0100

    Adds basic dish info component to frontend

---
+++ src/components/DishInfo.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import {
+  useAppSelector,
+  useAppDispatch,
+  selectSelectedDish,
+  getDishes,
+} from "../store";
+
+export const DishInfo = () => {
+  const dish = useAppSelector(selectSelectedDish);
+  const dispatch = useAppDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() =>
+          dispatch(getDishes({ id: "2032eec9-1b3e-42aa-b0a0-7215b2e844b4" }))
+        }
+      >
+        Get Seelachs
+      </button>
+      <button
+        onClick={() =>
+          dispatch(getDishes({ id: "903wee79-1b3e-32aa-c0a0-kek5b2e84439" }))
+        }
+      >
+        Get Pommes
+      </button>
+      <p>
+        {dish.id
+          ? "Selected Dish: " + dish.name + "has allergies:"
+          : "No dish selected"}
+      </p>
+      <ul>
+        {dish.allergies.map((elem) => (
+          <li key={elem.abbreviation}>{elem.abbreviation} </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
