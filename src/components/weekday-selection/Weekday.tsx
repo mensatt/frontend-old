f97bd05aabd8720089dfc9a45075c0b3dec63e3a
GamerBene19 <nerb.benedikt@gmail.com>
2022-02-12 08:47:30 +0100

    Adds navigation components

---
+++ src/components/weekday-selection/Weekday.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import dayjs from "dayjs";
+import styles from "./WeekdaySelection.module.css";
+
+type Props = {
+  date: dayjs.Dayjs;
+  selected?: boolean;
+  onClick: () => void;
+};
+
+const getDisplayText = (date: dayjs.Dayjs) => {
+  // TODO: Check if this is sth that dayjs can do
+  switch (date.weekday()) {
+    case 0:
+      return "Mo";
+    case 1:
+      return "Di";
+    case 2:
+      return "Mi";
+    case 3:
+      return "Do";
+    case 4:
+      return "Fr";
+  }
+};
+
+const getDisplayDate = (date: dayjs.Dayjs) => {
+  return date.date() < 10 ? "0" + date.date() : date.date();
+};
+
+const Weekday = ({ date, selected, onClick }: Props) => {
+  return (
+    <button
+      className={selected ? styles.selected : undefined}
+      onClick={onClick}
+    >
+      <p id="dow"> {getDisplayText(date)} </p>
+      <p id="date"> {getDisplayDate(date)} </p>
+    </button>
+  );
+};
+
+export default Weekday;
