fa086ba054329d861d32d15f22650ee843b30227
maanex <andreas@maanex.me>
2022-02-14 15:27:27 +0100

    add global scss variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: ['./src'],
+    prependData: `@import "~@styles/variables.scss";`
+  }
 }
