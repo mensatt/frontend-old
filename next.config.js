86ef40e5f1b8c94012de5883db72b26a68c5d34e
herkulessi <31536990+herkulessi@users.noreply.github.com>
2022-02-17 00:30:04 +0100

    Adds basic PWA support

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
-const path = require('path');
+const path = require("path");
+const withPWA = require("next-pwa");
 
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "~@styles/variables.scss";`,
   },
-};
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
