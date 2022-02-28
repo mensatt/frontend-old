abcf0b84eb1a2ea6b894acebcde11ec8ef4a60a6
GamerBene19 <nerb.benedikt@gmail.com>
2022-02-28 20:01:51 +0100

    Updates graphql-endpoints

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
   // TODO: Should have sth like an environment variable here
-  uri: 'http://localhost:5858/graphql',
+  uri: 'http://localhost:4000/v1/graphql',
   cache: new InMemoryCache({ addTypename: false, resultCaching: false }),
 });
 
