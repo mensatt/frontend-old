b2253e322bf983c9f10ceb4a09a1e52f91070159
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-05-13 15:16:33 +0200

    Add error handling to GQL-Query

--- src/store/actions/todays-dishes/getDIshesOfDate.ts
+++ src/store/actions/todays-dishes/getDIshesOfDate.ts
@@ -5,6 +5,7 @@ import {
   GetOccurrenceByDateQuery,
   GetOccurrenceByDateQueryVariables,
 } from '../../../graphql/graphql-types';
+import { setError } from '../error';
 
 import { setTodaysDishes } from '.';
 import { getOccurrenceByDate } from './queries.gql';
@@ -12,12 +13,18 @@ import { getOccurrenceByDate } from './queries.gql';
 export const getDishesOfDate = createAsyncThunk(
   'dishes/get',
   async ({ date }: GetOccurrenceByDateQueryVariables, { dispatch }) => {
-    const result = await apolloClient.query<GetOccurrenceByDateQuery>({
-      query: getOccurrenceByDate,
-      variables: {
-        date: date,
-      },
-    });
-    dispatch(setTodaysDishes(result.data.getOccurrencesByDate));
+    try {
+      const result = await apolloClient.query<GetOccurrenceByDateQuery>({
+        query: getOccurrenceByDate,
+        variables: {
+          date: date,
+        },
+      });
+      dispatch(setTodaysDishes(result.data.getOccurrencesByDate));
+    } catch (e) {
+      console.error(e);
+      dispatch(setTodaysDishes([]));
+      dispatch(setError(e));
+    }
   },
 );
