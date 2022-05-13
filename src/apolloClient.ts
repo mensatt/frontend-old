8495999d0bb541b8996e92e46ae6969953fd61fa
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-05-13 14:01:40 +0200

    Switch to Prod-API URL

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const apolloClient = new ApolloClient({
   // TODO: Should have sth like an environment variable here
-  uri: 'http://localhost:4000/v1/graphql',
+  uri: 'https://api.mensatt.de/v1/graphql',
   cache: new InMemoryCache({ addTypename: false, resultCaching: false }),
 });
 
