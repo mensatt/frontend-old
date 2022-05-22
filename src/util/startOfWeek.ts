dc0af0ab4b717d5d14ee2a9f69ed810a8cba971c
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-05-22 23:26:54 +0200

    Add week-overflow
    Also extracts dayjs to util file
    Closes #27

---
+++ src/util/startOfWeek.ts
@@ -0,0 +1,15 @@
+import dayjs from 'dayjs';
+import updateLocale from 'dayjs/plugin/updateLocale';
+import weekday from 'dayjs/plugin/weekday';
+
+dayjs.extend(weekday);
+dayjs.extend(updateLocale);
+dayjs.updateLocale('en', {
+  weekStart: 1,
+});
+
+export const currentWeekday = dayjs().weekday();
+export const afterFriday = currentWeekday > 4;
+export const startOfWeek = dayjs()
+  .add(afterFriday ? 1 : 0, 'week')
+  .weekday(0);
