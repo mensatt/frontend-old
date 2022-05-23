f4feaf533ebe66df068a0fa3f1f54ed6ce098a55
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-05-23 21:08:41 +0200

    Extract common components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import { Provider } from 'react-redux';
+import Footer from 'src/components/footer';
 
 import { store } from '../store';
 import '../styles/globals.scss';
@@ -8,6 +10,10 @@ import '../styles/globals.scss';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+      <Head>
+        <title>Mensatt</title>
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
       <div id="styling" className="theme-TODO">
         <div className={'container'}>
           <Component {...pageProps} />
