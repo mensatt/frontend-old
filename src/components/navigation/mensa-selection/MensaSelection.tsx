9a0d0847e019478087e5fd54aa9b74c79eabeb68
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-05-24 20:57:52 +0200

    Add option to toggle to dev-backend

--- src/components/navigation/mensa-selection/MensaSelection.tsx
+++ src/components/navigation/mensa-selection/MensaSelection.tsx
@@ -1,4 +1,10 @@
-import React from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
+import {
+  selectNavigation,
+  setActiveMensaIdx,
+  useAppDispatch,
+  useAppSelector,
+} from 'src/store';
 
 import styles from './MensaSelection.module.scss';
 
@@ -7,6 +13,39 @@ type Props = {
 };
 
 const MensaSelection = ({ className }: Props) => {
-  return <div className={className + ' ' + styles.content}>Südmensa</div>;
+  const dispatch = useAppDispatch();
+  const navigation = useAppSelector(selectNavigation);
+  const { activeMensaIdx, mensas } = navigation;
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // Read localStorage item if present
+      const cookeMensaIdx = mensas.findIndex(
+        (elem) => elem.url === localStorage.getItem('backendURL'),
+      );
+      // Dispatching is needed to get the information to the component below
+      if (cookeMensaIdx != -1) dispatch(setActiveMensaIdx(cookeMensaIdx));
+    }
+  }, [dispatch, mensas]);
+
+  const onMensaClick = useCallback(() => {
+    if (typeof window !== 'undefined') {
+      // On client, set localStorage item
+      localStorage.setItem('backendURL', mensas[(activeMensaIdx + 1) % 2].url);
+      // Reloading is needed to "apply" change (Apollo would cache otherwise)
+      location.reload();
+    }
+  }, [activeMensaIdx, mensas]);
+
+  const activeMensa = useMemo(
+    () => mensas[activeMensaIdx],
+    [activeMensaIdx, mensas],
+  );
+
+  return (
+    <div className={className + ' ' + styles.content}>
+      <p onClick={onMensaClick}>{activeMensa.name}</p>
+    </div>
+  );
 };
 export default MensaSelection;
--- src/store/actions/navigation/index.ts
+++ src/store/actions/navigation/index.ts
@@ -2,12 +2,32 @@ import { createSlice } from '@reduxjs/toolkit';
 
 import { RootState } from '../../store';
 
+// As of writing each mensa had its own backend-URL. This was done so you can switch to the dev-backend on the fly.
+// TODO: In the future this should not be done this way, but rather by using a query parameter (where necessary)
+type Mensa = {
+  name: string;
+  url: string;
+};
+
 type NavigationState = {
   weekday: number;
+  activeMensaIdx: number;
+  mensas: Mensa[];
 };
 
 const initialState: NavigationState = {
   weekday: 0,
+  activeMensaIdx: 0,
+  mensas: [
+    {
+      name: 'Südmensa',
+      url: 'https://api.mensatt.de/v1/graphql',
+    },
+    {
+      name: 'Devmensa',
+      url: 'https://dev-api.mensatt.de/v1/graphql',
+    },
+  ],
 };
 
 export const navigationSlice = createSlice({
@@ -17,10 +37,13 @@ export const navigationSlice = createSlice({
     setWeekday: (state, action) => {
       state.weekday = action.payload;
     },
+    setActiveMensaIdx: (state, action) => {
+      state.activeMensaIdx = action.payload;
+    },
   },
 });
 
-export const { setWeekday } = navigationSlice.actions;
+export const { setWeekday, setActiveMensaIdx } = navigationSlice.actions;
 
 export const selectNavigation = (state: RootState) => state.navigation;
 
