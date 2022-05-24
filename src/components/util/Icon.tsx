54c442e6e6bde20e8d10caf3b4eaac15f63b974c
maanex <andreas@maanex.me>
2022-05-24 14:21:17 +0200

    improve navigation

--- src/components/util/Icon.tsx
+++ src/components/util/Icon.tsx
@@ -1,7 +1,9 @@
+import ARROW_LEFT from '../../assets/icons/arrow_left.svg';
 import FLAG_DE from '../../assets/icons/flag_de.svg';
 import FLAG_US from '../../assets/icons/flag_us.svg';
 
 const icons = {
+  arrow_left: <ARROW_LEFT />,
   flag_de: <FLAG_DE />,
   flag_us: <FLAG_US />,
 };
