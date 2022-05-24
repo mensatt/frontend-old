5cc4b3862e35f90141cd27d21f03a2faffe1b066
maanex <andreas@maanex.me>
2022-05-24 13:59:08 +0200

    improve navigation

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,20 +1,45 @@
 import React from 'react';
 
 import styles from './Navigation.module.scss';
+import BackButton from './back-button';
 import LanguageSwitcher from './language-switcher';
 import MensaSelection from './mensa-selection';
 import WeekdaySelection from './weekday-selection';
 
-const Navigation = () => {
+export type NavigaitonDisplayOptions = {
+  action: 'weekday-selection' | 'back-button';
+  brand: boolean;
+  mensa: boolean;
+  language: boolean;
+};
+
+type Props = {
+  opts: NavigaitonDisplayOptions;
+};
+
+const Navigation = ({ opts }: Props) => {
+  const settingsItems = [
+    opts.mensa ? <MensaSelection /> : null,
+    opts.language ? <LanguageSwitcher /> : null,
+  ].filter((i) => !!i) as JSX.Element[];
+
+  // insert divider divs between each item in the list
+  const settingsRendered = settingsItems
+    .flatMap((item, index) => [
+      item,
+      <div className={styles.divider} key={index} />,
+    ])
+    .flat()
+    .slice(0, -1);
+
   return (
     <nav className={styles.content}>
-      <WeekdaySelection />
-      <h1>Mensatt</h1>
-      <div className={styles.settings}>
-        <MensaSelection />
-        <div className={styles.divider} />
-        <LanguageSwitcher />
+      <div className={styles.navaction}>
+        {opts.action === 'weekday-selection' ? <WeekdaySelection /> : ''}
+        {opts.action === 'back-button' ? <BackButton /> : ''}
       </div>
+      {opts.brand ? <h1>Mensatt</h1> : ''}
+      <div className={styles.settings}>{settingsRendered}</div>
     </nav>
   );
 };
--- src/components/util/Icon.tsx
+++ src/components/util/Icon.tsx
@@ -1,9 +1,11 @@
+import ARROW_LEFT from '../../assets/icons/arrow_left.svg';
 import FLAG_DE from '../../assets/icons/flag_de.svg';
 import FLAG_US from '../../assets/icons/flag_us.svg';
 
 const icons = {
   flag_de: <FLAG_DE />,
   flag_us: <FLAG_US />,
+  arrow_left: <ARROW_LEFT />,
 };
 
 type Props = {
