b4c5a50e536500aa4d09d13a0ad13ed0bfc8432f
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-05-24 20:49:26 +0200

    Add localStorage based backend URL selection

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,8 +1,28 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+  concat,
+} from '@apollo/client';
+
+const authMiddleware = new ApolloLink((operation, forward) => {
+  // TODO: Add Auth
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  operation.setContext(({ headers = {} }) => ({
+    // headers: {
+    //   ...headers,
+    //   authorization: localStorage.getItem('token') || null,
+    // },
+    uri:
+      localStorage.getItem('backendURL') || 'https://api.mensatt.de/v1/graphql',
+  }));
+
+  return forward(operation);
+});
 
 const apolloClient = new ApolloClient({
-  // TODO: Should have sth like an environment variable here
-  uri: 'https://api.mensatt.de/v1/graphql',
+  link: concat(authMiddleware, new HttpLink()),
   cache: new InMemoryCache({ addTypename: false, resultCaching: false }),
 });
 
