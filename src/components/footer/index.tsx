09b78dbbbae93d9f9ec2ddc07d275025ec52200b
maanex <andreas@maanex.me>
2022-05-24 11:38:49 +0200

    clean up footer and some js

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,15 +1,18 @@
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 import React from 'react';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
   const { t } = useTranslation('footer');
 
   return (
-    <footer>
-      <Link href={'/contact'}>{t('contact-link-text')}</Link>
-      <Link href={'/privacy'}>{t('privacy-link-text')}</Link>
+    <footer className={styles.footer}>
       <p>{t('disclaimer')}</p>
+      <div className={styles.links}>
+        <Link href={'/contact'}>{t('contact-link-text')}</Link>
+        <Link href={'/privacy'}>{t('privacy-link-text')}</Link>
+      </div>
     </footer>
   );
 };
