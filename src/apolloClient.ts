872f884355058c3188f0a9abe960eba7036ba628
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-06-03 00:02:12 +0200

    Add support for token auth

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,28 +1,20 @@
-import {
-  ApolloClient,
-  ApolloLink,
-  HttpLink,
-  InMemoryCache,
-  concat,
-} from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-const authMiddleware = new ApolloLink((operation, forward) => {
-  // TODO: Add Auth
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  operation.setContext(({ headers = {} }) => ({
-    // headers: {
-    //   ...headers,
-    //   authorization: localStorage.getItem('token') || null,
-    // },
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
     uri:
       localStorage.getItem('backendURL') || 'https://api.mensatt.de/v1/graphql',
-  }));
-
-  return forward(operation);
+  };
 });
 
 const apolloClient = new ApolloClient({
-  link: concat(authMiddleware, new HttpLink()),
+  link: authLink.concat(new HttpLink()),
   cache: new InMemoryCache({ addTypename: false, resultCaching: false }),
 });
 
