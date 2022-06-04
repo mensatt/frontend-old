6afa5f5db8c59468fb73b26f6e0b5dc3abb0e948
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-06-04 10:11:01 +0200

    Use memoization in Navigation

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useMemo } from 'react';
 
 import styles from './Navigation.module.scss';
 import LanguageSwitcher from './language-switcher';
@@ -16,18 +16,20 @@ type Props = {
 };
 
 const Navigation = ({ opts }: Props) => {
-  const settingsItems = [];
-  if (opts.showMensa) settingsItems.push(<MensaSelection key={'showMensa'} />);
-  if (opts.showLanguage)
-    settingsItems.push(<LanguageSwitcher key={'showLanguage'} />);
-
-  // insert divider divs between each item in the list
-  const settingsRendered = settingsItems
-    .flatMap((item, index) => [
-      item,
-      <div className={styles.divider} key={index} />,
-    ])
-    .slice(0, -1);
+  const settingsRendered = useMemo(() => {
+    const array = [];
+    if (opts.showMensa) array.push(<MensaSelection key={'showMensa'} />);
+    if (opts.showLanguage)
+      array.push(<LanguageSwitcher key={'showLanguage'} />);
+    // insert divider divs between each item in the list
+    array
+      .flatMap((item, index) => [
+        item,
+        <div className={styles.divider} key={index} />,
+      ])
+      .slice(0, -1);
+    return array;
+  }, [opts.showLanguage, opts.showMensa]);
 
   return (
     <nav className={styles.content}>
