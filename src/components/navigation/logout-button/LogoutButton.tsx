54bc58ed41832277dc78758bf1956dc33a416fc6
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-06-09 00:06:52 +0200

    Add popup to indicate successful logout

--- src/components/navigation/logout-button/LogoutButton.tsx
+++ src/components/navigation/logout-button/LogoutButton.tsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import { useTranslation } from 'next-i18next';
+import React, { useEffect, useState } from 'react';
+import Popup from 'reactjs-popup';
 import { isLoggedInVar } from 'src/apollo';
 import Icon from 'src/components/icon';
 import { GET_NAVIGATION, Navigation } from 'src/graphql/queries';
@@ -8,6 +10,7 @@ import { useLazyQuery } from '@apollo/client';
 import styles from './LogoutButton.module.scss';
 
 const LogoutButton = () => {
+  const { t } = useTranslation('common');
   const [getNavigation, { data }] = useLazyQuery<Navigation>(GET_NAVIGATION);
 
   // We have to lazily call the query after the first page load to ensure proper hydration
@@ -15,18 +18,34 @@ const LogoutButton = () => {
     getNavigation();
   }, [getNavigation]);
 
+  const [popupOpen, setPopupOpen] = useState(false);
+
   return (
     <>
-      {data && data.isLoggedIn && (
-        <button
-          onClick={() => {
+      {data && (
+        <Popup
+          position="bottom right"
+          on="click"
+          closeOnDocumentClick
+          open={popupOpen}
+          onOpen={() => {
             localStorage.removeItem('token');
             isLoggedInVar(false);
+            setPopupOpen(true);
+            setTimeout(() => setPopupOpen(false), 1500);
           }}
-          className={styles.button}
+          trigger={
+            data.isLoggedIn ? (
+              <button className={styles.button}>
+                <Icon name={'logout'} />
+              </button>
+            ) : (
+              <></>
+            )
+          }
         >
-          <Icon name={'logout'} />
-        </button>
+          <p>{t('logoutSuccessful')}</p>
+        </Popup>
       )}
     </>
   );
