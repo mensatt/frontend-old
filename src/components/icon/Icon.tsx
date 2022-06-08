83635168c7300399f71dc7dcfbf6654f07eefa41
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-06-08 22:47:43 +0200

    Add logout icon

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,11 +1,13 @@
 import ARROW_LEFT from '../../assets/icons/arrow_left.svg';
 import FLAG_DE from '../../assets/icons/flag_de.svg';
 import FLAG_US from '../../assets/icons/flag_us.svg';
+import LOGOUT from '../../assets/icons/logout.svg';
 
 const icons = {
   arrow_left: <ARROW_LEFT />,
   flag_de: <FLAG_DE />,
   flag_us: <FLAG_US />,
+  logout: <LOGOUT />,
 };
 
 type Props = {
