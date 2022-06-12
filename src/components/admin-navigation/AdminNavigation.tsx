c4e81dead9ef3ec694f3a81e310b9212c2f5ced6
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-06-12 22:09:27 +0200

    Remove incomplete localization for admin panel

--- src/components/admin-navigation/AdminNavigation.tsx
+++ src/components/admin-navigation/AdminNavigation.tsx
@@ -1,15 +1,13 @@
-import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
 import React from 'react';
 
 const AdminNavigation = () => {
-  const { t } = useTranslation('admin');
   return (
     <div>
-      <Link href={'/admin/'}>{t('indexLink')}</Link>
-      <Link href={'/admin/occurrences'}>{t('occurrencesLink')}</Link>
-      <Link href={'/admin/dishes'}>{t('dishesLink')}</Link>
-      <Link href={'/admin/reviews'}>{t('reviewsLink')}</Link>
+      <Link href={'/admin/'}>Overview</Link>
+      <Link href={'/admin/occurrences'}>Occurrences</Link>
+      <Link href={'/admin/dishes'}>Dishes</Link>
+      <Link href={'/admin/reviews'}>Reviews</Link>
     </div>
   );
 };
