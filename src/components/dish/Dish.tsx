45423fa9ca579f12368ce1fb59fc27bbdeceab6c
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-07-12 21:26:08 +0200

    Refactor Dish comp to take occurrence

--- src/components/dish/Dish.tsx
+++ src/components/dish/Dish.tsx
@@ -1,12 +1,15 @@
 import { useTranslation } from 'next-i18next';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { useMemo } from 'react';
+import { GetOccurrencesByDateQuery } from 'src/graphql/graphql-types';
 
 import styles from './Dish.module.scss';
 import DishComment from './comment';
 import { Props as CommentProps } from './comment/DishComment';
 
 type Props = {
-  name: string;
+  occurrence: GetOccurrencesByDateQuery['occurrencesByDate'][number];
 };
 
 const dummyComments: Array<CommentProps> = [
@@ -16,8 +19,14 @@ const dummyComments: Array<CommentProps> = [
   { author: 'sit', text: 'Lorem ipsum dolor sit amet!' },
 ];
 
-const Dish = ({ name }: Props) => {
+const Dish = ({ occurrence }: Props) => {
   const { t } = useTranslation('common');
+  const { locale: routerLocale } = useRouter();
+  const locale = useMemo(
+    () => (routerLocale ? routerLocale : 'de'),
+    [routerLocale],
+  );
+
   const comments = dummyComments.map((elem) => (
     <DishComment
       key={elem.author + elem.text}
@@ -25,18 +34,31 @@ const Dish = ({ name }: Props) => {
       text={elem.text}
     />
   ));
+
+  const occurrenceName = useMemo(
+    () =>
+      // Fallback to german value if no english value is present
+      locale === 'en'
+        ? occurrence.dish.nameEn ?? occurrence.dish.nameDe
+        : occurrence.dish.nameDe,
+    [locale, occurrence.dish.nameDe, occurrence.dish.nameEn],
+  );
+
   return (
     <div className={styles.content}>
       <div className={styles.image}>
         <Image
           src={'https://picsum.photos/1400/600'}
-          alt={t('imageDescription', { name: name, author: 'John Doe' })}
+          alt={t('imageDescription', {
+            name: occurrenceName,
+            author: 'John Doe',
+          })}
           // width={100}
           // height={600}
           layout={'fill'}
         />
       </div>
-      <h2>{name}</h2>
+      <h2>{occurrenceName}</h2>
       <h3>{t('commentHeading')}</h3>
       {comments}
       <button className={styles.allComments}>{t('showAllComments')}</button>
