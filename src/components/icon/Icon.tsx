537801ea9bd718dfd0be28f50e9aa7b04fc10e0d
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-07-13 22:55:50 +0200

    Add possibilty to use react componets as icons

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,3 +1,6 @@
+import { useMemo } from 'react';
+
+import StarIcon, { StarIconProps } from '../../assets/icons/Star';
 import ARROW_LEFT from '../../assets/icons/arrow_left.svg';
 import FLAG_DE from '../../assets/icons/flag_de.svg';
 import FLAG_US from '../../assets/icons/flag_us.svg';
@@ -8,12 +11,23 @@ const icons = {
   flag_de: <FLAG_DE />,
   flag_us: <FLAG_US />,
   logout: <LOGOUT />,
+  star: (args: StarIconProps) => <StarIcon {...args} />,
 };
 
 type Props = {
   name: keyof typeof icons;
+  compProps?: object;
 };
 
-const Icon = ({ name }: Props) => <div className="icon">{icons[name]}</div>;
+const Icon = ({ name, compProps }: Props) => {
+  const icon = useMemo(() => {
+    const selectedIcon = icons[name];
+    if (typeof selectedIcon === 'object') return selectedIcon;
+    else if (typeof selectedIcon === 'function') {
+      return selectedIcon(compProps ?? {});
+    }
+  }, [compProps, name]);
+  return <div className="icon">{icon}</div>;
+};
 
 export default Icon;
