ef536daece08a93a4112d3424d095d2b5c3fb252
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-07-22 20:31:24 +0200

    refactor(gql): simplify export path for mutations

--- src/graphql/mutations/index.ts
+++ src/graphql/mutations/index.ts
@@ -1,3 +1,3 @@
-export * from '../mutations/addReview.gql';
-export * from '../mutations/loginUser.gql';
+export * from './addReview.gql';
+export * from './loginUser.gql';
 export * from './setOccurrenceReviewStatus.gql';
