958cb23715ff367c327f2851aa29e5c09d692557
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-07-25 17:26:20 +0200

    feat: add extension check for images

--- src/components/occurrence/review-modal/ReviewModal.tsx
+++ src/components/occurrence/review-modal/ReviewModal.tsx
@@ -23,6 +23,7 @@ import styles from './ReviewModal.module.scss';
 
 // Maximum allowed upload filesize (in Byte)
 const MAX_UPLOAD_FILE_SIZE = 10 * 1024 * 1024;
+const ALLOWED_EXTENSIONS = ['jpeg', 'jpg', 'png', 'webp'];
 
 type Props = {
   occurrenceName:
@@ -48,6 +49,7 @@ const ReviewModal = ({
   const [showMissingStarValueError, setShowMissingStarValueError] =
     useState(true);
   const [showImageTooBigError, setShowImageTooBigError] = useState(false);
+  const [showWrongExtensionError, setShowWrongExtensionError] = useState(false);
 
   useEffect(() => {
     if (addReviewData) {
@@ -64,8 +66,8 @@ const ReviewModal = ({
   });
 
   const formIsInvalid = useMemo(
-    () => showMissingStarValueError,
-    [showMissingStarValueError],
+    () => showMissingStarValueError || showWrongExtensionError,
+    [showMissingStarValueError, showWrongExtensionError],
   );
 
   const handleSubmit = useCallback(
@@ -86,11 +88,26 @@ const ReviewModal = ({
         // `FileList` is not an array so we have to "convert" it
         const fileList = Array.from(event.target.files);
 
+        // This function is from here: https://stackoverflow.com/a/12900504
+        const getFileExt = (file: File) =>
+          file.name.slice(((file.name.lastIndexOf('.') - 1) >>> 0) + 2);
+
+        // Check filesize
         if (fileList.every((file) => file.size <= MAX_UPLOAD_FILE_SIZE)) {
-          setFormState({
-            ...formState,
-            images: fileList.map((file) => ({ image: file })),
-          });
+          // Check file extension
+          if (
+            fileList.every((file) =>
+              ALLOWED_EXTENSIONS.includes(getFileExt(file).toLowerCase()),
+            )
+          ) {
+            setFormState({
+              ...formState,
+              images: fileList.map((file) => ({ image: file })),
+            });
+            setShowWrongExtensionError(false);
+          } else {
+            setShowWrongExtensionError(true);
+          }
           setShowImageTooBigError(false);
         } else {
           setShowImageTooBigError(true);
@@ -113,6 +130,15 @@ const ReviewModal = ({
         <p>
           {t('reviewModalImageTooBigError', {
             size: `${MAX_UPLOAD_FILE_SIZE / (1024 * 1024)}MB`,
+          })}
+        </p>
+      )}
+      {showWrongExtensionError && (
+        <p>
+          {t('reviewModalWrongExtensionError', {
+            extensions: ALLOWED_EXTENSIONS.map(
+              (ext) => '.' + ext.toLowerCase(),
+            ).join(' '),
           })}
         </p>
       )}
