320e440c346bf3b2e3d731c3b04984aace5c840e
stormofice <58337328+stormofice@users.noreply.github.com>
2022-07-26 16:59:35 +0200

    feat(Admin Panel): Show dishes in admin panel

--- src/pages/admin/dishes.tsx
+++ src/pages/admin/dishes.tsx
@@ -1,9 +1,44 @@
 import { NextPage } from 'next';
+import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { GetAdminPanelDishesQuery } from 'src/graphql/graphql-types';
+import { GET_ADMIN_PANEL_DISHES } from 'src/graphql/queries/';
+
+import { useQuery } from '@apollo/client';
+
+import Table, { TableDataRow, TableHeaderRow } from '../../components/table';
 
 const AdminDishesPage: NextPage = () => {
-  return <div>AdminDishesPage</div>;
+  const { t } = useTranslation('common');
+
+  const [headerRows] = useState<TableHeaderRow[]>([
+    { fieldName: 'id', displayName: 'ID' },
+    { fieldName: 'nameDe', displayName: 'German name' },
+    { fieldName: 'nameEn', displayName: 'English name' },
+    { fieldName: 'aliases', displayName: 'Aliases' },
+  ]);
+
+  const { data, loading, error } = useQuery<GetAdminPanelDishesQuery>(
+    GET_ADMIN_PANEL_DISHES,
+  );
+  const rows: TableDataRow[] = useMemo(() => {
+    if (!data || data.dishes.length === 0) return [];
+    return data.dishes.map((elem) => ({
+      id: elem.id,
+      nameDe: elem.nameDe ?? '<NULL>',
+      nameEn: elem.nameEn ?? '<NULL>',
+      aliases: elem.aliases.join(','),
+    }));
+  }, [data]);
+
+  return (
+    <>
+      {loading && t('loading')}
+      {error && error.message}
+      {data && <Table headerRow={headerRows} dataRows={rows} />}
+    </>
+  );
 };
 
 export default AdminDishesPage;
