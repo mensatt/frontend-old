cd969df96220471a5ab9687b7241b2f803fea180
stormofice <58337328+stormofice@users.noreply.github.com>
2022-07-27 18:52:34 +0200

    feat(Admin Panel): Create update dish mutation

--- src/graphql/mutations/index.ts
+++ src/graphql/mutations/index.ts
@@ -2,3 +2,4 @@ export * from './addReview.gql';
 export * from './loginUser.gql';
 export * from './setReviewApprovalStatus.gql';
 export * from './setOccurrenceStatus.gql';
+export * from './updateDish.gql';
