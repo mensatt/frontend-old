f3d16b279760f772e7eb3cd85ca2e31c61053879
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-08-01 14:54:00 +0200

    refactor(weekday-seleciton): clean up code

--- src/components/navigation/weekday-selection/WeekdaySelection.tsx
+++ src/components/navigation/weekday-selection/WeekdaySelection.tsx
@@ -1,32 +1,66 @@
 import dayjs from 'dayjs';
 import { NextRouter, useRouter } from 'next/router';
-import React, { useCallback, useEffect, useMemo } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { selectedDateVar } from 'src/apollo';
-import { GET_NAVIGATION, Navigation } from 'src/graphql/queries';
-import { DATE_FORMAT, afterFriday, currentDate, startOfWeek } from 'src/util/';
+import {
+  DATE_FORMAT,
+  afterFriday,
+  currentDate,
+  getStartOfWeek,
+} from 'src/util/';
 
-import { useQuery } from '@apollo/client';
+import { useReactiveVar } from '@apollo/client';
 
 import Weekday from './Weekday';
 import styles from './WeekdaySelection.module.scss';
 
 const WeekdaySelection = () => {
-  const { data } = useQuery<Navigation>(GET_NAVIGATION);
   const router = useRouter();
+
+  const selectedDateString = useReactiveVar(selectedDateVar);
+  const selectedDate = useMemo(
+    () => dayjs(selectedDateString, DATE_FORMAT),
+    [selectedDateString],
+  );
+  const selectedDateVarWrapper = useCallback(
+    (date: dayjs.Dayjs) => selectedDateVar(date.format(DATE_FORMAT)),
+    [],
+  );
+
+  const [dateChangedManually, setDateChangedManually] = useState(false);
+
+  useEffect(() => {
+    if (!router.isReady) return;
+    if (dateChangedManually)
+      router.push(
+        {
+          pathname: router.pathname,
+          query: {
+            date: selectedDate.format(DATE_FORMAT),
+          },
+        },
+        undefined,
+        { shallow: true },
+      );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dateChangedManually, selectedDate]);
 
   // Computes the date that is set by the `useEffect` below
   const determineDateToSet = useCallback(
-    (
-      dateUrl: NextRouter['query'][string],
-      afterFriday: boolean,
-      startOfWeek: dayjs.Dayjs,
-    ) => {
+    (dateUrl: NextRouter['query'][string], afterFriday: boolean) => {
       const dateUrlParsed =
         typeof dateUrl === 'string'
           ? dayjs(dateUrl, DATE_FORMAT, true)
           : undefined;
-      const nextOpenDate = afterFriday ? startOfWeek : currentDate;
-      return dateUrlParsed?.isValid() ? dateUrlParsed : nextOpenDate;
+      const nextMonday = currentDate.add(1, 'week').weekday(0);
+
+      // Priority is as follows
+      //  - date from URL (if present (and valid))
+      //  - next monday (if currentDate is after friday)
+      //  - currentDate
+      if (dateUrlParsed?.isValid()) return dateUrlParsed;
+      if (afterFriday) return nextMonday;
+      return currentDate;
     },
     [],
   );
@@ -41,35 +75,27 @@ const WeekdaySelection = () => {
     const { date: dateURL } = router.query;
 
     selectedDateVar(
-      determineDateToSet(dateURL, afterFriday, startOfWeek).format(DATE_FORMAT),
+      determineDateToSet(dateURL, afterFriday).format(DATE_FORMAT),
     );
   }, [determineDateToSet, router]);
 
   const week = useMemo(
     () =>
-      data &&
-      [0, 1, 2, 3, 4].map((elem) => {
-        const thisWeekdaysDate = startOfWeek.add(elem, 'day');
-        const thisWeekdaysDateString = thisWeekdaysDate.format(DATE_FORMAT);
+      [0, 1, 2, 3, 4].map((_, idx) => {
+        const thisWeekdaysDate = getStartOfWeek(selectedDate).add(idx, 'day');
         return (
           <Weekday
-            key={elem}
+            key={idx}
             date={thisWeekdaysDate}
-            selected={thisWeekdaysDateString === data.selectedDate}
-            onClick={() =>
-              router.push(
-                {
-                  pathname: router.pathname,
-                  query: { date: thisWeekdaysDateString },
-                },
-                undefined,
-                { shallow: true },
-              )
-            }
+            selected={thisWeekdaysDate.isSame(selectedDate, 'day')}
+            onClick={() => {
+              setDateChangedManually(true);
+              selectedDateVarWrapper(thisWeekdaysDate);
+            }}
           />
         );
       }),
-    [data, router],
+    [selectedDate, selectedDateVarWrapper],
   );
 
   return <div className={styles.container}>{week}</div>;
