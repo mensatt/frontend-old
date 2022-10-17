73556bdacd0c5d2c097b0aea1ecd745af21afb1c
Benedikt Nerb <nerb.benedikt@gmail.com>
2022-10-18 01:17:25 +0200

    chore: update next pwa config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 const path = require('path');
-const withPWA = require('next-pwa');
 const { i18n } = require('./next-i18next.config');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
@@ -8,12 +13,6 @@ module.exports = withPWA({
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
     prependData: '@import "~@styles/variables.scss";',
-  },
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development',
   },
   images: { domains: ['dev-api.mensatt.de', 'api.mensatt.de'] },
   i18n,
